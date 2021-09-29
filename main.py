d9f3829383863eeee14b9e5281af039699bea724
carol <awdrg1210@gmail.com>
2021-09-29 12:59:35 +0800

    fastapi with sqlalchemy

--- main.py
+++ main.py
@@ -1,64 +1,28 @@
-from typing import Optional
+from typing import List
 
-from fastapi import FastAPI, Request
-from fastapi.encoders import jsonable_encoder
+from fastapi import Depends, FastAPI, Request
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
 
-from exceptions import NotFoundError
+from restapi.crud import crud_user
+from restapi.db.sqlite import SessionLocal, engine
+from restapi.exceptions import NotFoundError
+from restapi.models import user as UserModel
+from restapi.schemas.user import User, UserCreate, UserUpdate
+
+# https://stackoverflow.com/a/68383073
+UserModel.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-class UserBase(BaseModel):
-
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-
-
-class UserIn(UserBase):
-
-    password: str = Field(..., min_length=5)
-
-
-class UserOut(UserBase):
-
-    pass
-
-
-class UserInDB(UserBase):
-
-    hashed_password: str
-
-
-class UserUpdate(BaseModel):
-
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    password: str = Field(None, min_length=5)
-
-
-fake_db = {
-    "ray": {
-        "user_id": "a001",
-        "username": "ray",
-        "hashed_password": "supersecretraypass"
-    },
-    "will": {
-        "user_id": "a002",
-        "username": "will",
-        "email": "will@gmail.com",
-        "hashed_password": "supersecretwillpass"
-    },
-    "ted": {
-        "user_id": "a003",
-        "username": "ted",
-        "email": "ted@gmail.com",
-        "full_name": "ted ho",
-        "hashed_password": "supersecrettedpass"
-    },
-}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.exception_handler(NotFoundError)
@@ -69,63 +33,66 @@ async def not_found_error_handler(request: Request, exc: NotFoundError):
     )
 
 
-def fake_password_hasher(raw_password: str):
-    return "supersecret" + raw_password
-
-
-def fake_save_user(user: UserBase, hashed_password: str):
-    user_in_db = UserInDB(**user.dict(), hashed_password=hashed_password)
-    print("User saved! ..not really")
-    return user_in_db
-
-
-@app.get("/user/{username}")
-async def get_user(username: str, response_model=UserOut):
-    if username not in fake_db:
+@app.get("/user/{username}", response_model=User)
+async def get_user(
+    username: str,
+    db: Session = Depends(get_db)
+):
+    db_user = crud_user.get_user_by_username(db=db, username=username)
+    if db_user is None:
         raise NotFoundError(message="User not found")
-    return fake_db[username]
+    return db_user
 
 
-@app.post("/user/", response_model=UserOut, status_code=201)
-async def create_user(user_in: UserIn):
-    if user_in.username in fake_db:
+@app.get("/users/", response_model=List[User])
+def get_users(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db)
+):
+    users = crud_user.get_users(db=db, skip=skip, limit=limit)
+    return users
+
+
+@app.post("/user/", response_model=User, status_code=201)
+async def create_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    db_user = crud_user.get_user_by_username(db=db, username=user.username)
+    if db_user:
         return JSONResponse(
             status_code=400,
             content={"message": "User already exist"}
         )
-    hashed_password = fake_password_hasher(user_in.password)
-    user_saved = fake_save_user(user_in, hashed_password)
-    fake_db[user_in.username] = jsonable_encoder(user_saved)
-    return user_saved
+    return crud_user.create_user(db=db, user=user)
 
 
-@app.patch("/user/{username}", response_model=UserOut)
-async def update_user(username: str, user_update: UserUpdate):
-    if username not in fake_db:
+@app.patch("/user/{username}")
+async def update_user(
+    username: str,
+    updates: UserUpdate,
+    db: Session = Depends(get_db)
+):
+    db_user = crud_user.get_user_by_username(db=db, username=username)
+    if db_user is None:
         raise NotFoundError(message="User not found")
-    stored_user_data = fake_db[username]
-    stored_user_update_model = UserUpdate(**stored_user_data)
-    update_data = user_update.dict(exclude_unset=True)
-    updated_data = stored_user_update_model.copy(update=update_data)
-    if updated_data.password:
-        hashed_password = fake_password_hasher(
-            raw_password=updated_data.password
-        )
-    else:
-        hashed_password = stored_user_data["hashed_password"]
-    user_saved = fake_save_user(
-        user=UserBase(**updated_data.dict(), username=username),
-        hashed_password=hashed_password
+    crud_user.update_user(db=db, db_obj=db_user, updates=updates)
+    return JSONResponse(
+        status_code=200,
+        content={"message": "Update user success"}
     )
-    fake_db[username] = jsonable_encoder(user_saved)
-    return user_saved
 
 
 @app.delete("/user/{username}")
-async def delete_user(username: str):
-    if username not in fake_db:
+async def delete_user(
+    username: str,
+    db: Session = Depends(get_db)
+):
+    db_user = crud_user.get_user_by_username(db=db, username=username)
+    if db_user is None:
         raise NotFoundError(message="User not found")
-    fake_db.pop(username)
+    crud_user.delete_user(db=db, db_obj=db_user)
     return JSONResponse(
         status_code=200,
         content={"message": f"Deleted user {username}"}
---
+++ restapi/schemas/user.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class UserBase(BaseModel):
+
+    username: str
+    email: str
+    full_name: Optional[str] = None
+
+
+class UserCreate(UserBase):
+
+    password: str = Field(..., min_length=5, max_length=16)
+
+
+class UserInDB(UserBase):
+
+    hashed_password: str
+
+
+class User(UserBase):
+
+    user_id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+
+class UserUpdate(BaseModel):
+
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    password: str = Field(None, min_length=5, max_length=16)
