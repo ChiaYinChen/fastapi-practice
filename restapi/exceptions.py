17c1c81d4822166e200337ded1f8664d1f423337
carol <awdrg1210@gmail.com>
2021-10-01 11:34:45 +0800

    add not_found_error_handler

--- restapi/exceptions.py
+++ restapi/exceptions.py
@@ -1,11 +1,19 @@
 """Exception for API endpoints."""
+from fastapi import Request
+from fastapi.responses import JSONResponse
 
 
 class NotFoundError(Exception):
     """Resource not found error."""
 
-    def __init__(self, message, error_number=None):
+    def __init__(self, message):
         """Init."""
         self.message = message
         self.status_code = 404
-        self.error_number = error_number
+
+
+async def not_found_error_handler(request: Request, exc: NotFoundError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.message},
+    )
