478104929fdcc0f8cd0dac0608934190256d3704
carol <awdrg1210@gmail.com>
2021-10-01 16:35:15 +0800

    redefine 'respapi' to 'app'

--- app/db/sqlite.py
+++ app/db/sqlite.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./restapi/demo.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app/demo.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
