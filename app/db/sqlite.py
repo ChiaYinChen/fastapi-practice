dfee6baac78f07c70fecb9e9e0f74fb367180c22
carol <awdrg1210@gmail.com>
2021-10-04 15:03:12 +0800

    move declarative_base to base_class.py

--- app/db/sqlite.py
+++ app/db/sqlite.py
@@ -1,18 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app/demo.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={"check_same_thread": False}  # required for sqlite
 )
+# https://stackoverflow.com/a/63650364
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-# https://stackoverflow.com/a/63650364
-
-Base = declarative_base()
