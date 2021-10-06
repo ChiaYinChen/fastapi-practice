d0858feaab98a60d434c3e8e1d730540dc669ccb
carol <awdrg1210@gmail.com>
2021-10-06 11:48:43 +0800

    JWT auth endpoint - login flow

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from .db.sqlite import engine
 from .exceptions import NotFoundError, not_found_error_handler
 from .models import user as UserModel
-from .routers import user, users
+from .routers import login, user, users
 
 # https://stackoverflow.com/a/68383073
 UserModel.Base.metadata.create_all(bind=engine)
@@ -12,5 +12,6 @@ app = FastAPI()
 
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(login.router, tags=["login"])
 
 app.add_exception_handler(NotFoundError, not_found_error_handler)
---
+++ app/routers/login.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from .. import crud
+from ..core.security import create_access_token
+from ..dependencies import get_db
+from ..schemas.token import Token
+
+router = APIRouter()
+
+
+@router.post("/login/access-token", response_model=Token)
+def login_access_token(
+    db: Session = Depends(get_db),
+    form_data: OAuth2PasswordRequestForm = Depends()
+) -> Any:
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = crud.user.authenticate(
+        db=db, username=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")  # noqa: E501
+    elif not crud.user.is_active(user):
+        raise HTTPException(status_code=401, detail="Inactive user")
+    return {
+        "access_token": create_access_token(sub=user.username),
+        "token_type": "bearer",
+    }
