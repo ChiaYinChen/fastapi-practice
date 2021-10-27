aedbc479d795776cf52e32bba083988b650404a5
carol <awdrg1210@gmail.com>
2021-10-27 12:31:48 +0800

    set custom logging handler for pytest

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,10 +1,12 @@
 """Pytest's conftest.py."""
+import logging
 from typing import Dict, Generator
 
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
+from app import color_formatter
 from app.core.config import settings
 from app.db.session import SessionLocal
 from app.main import app
@@ -33,3 +35,13 @@ def normal_user_token_headers(
     return authentication_token_from_username(
         client=client, username=settings.TEST_USER_USERNAME, db=db
     )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_logging():
+    """Set custom logging handler for pytest."""
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    _handler = logging.StreamHandler()
+    _handler.setFormatter(color_formatter)
+    logger.addHandler(_handler)
